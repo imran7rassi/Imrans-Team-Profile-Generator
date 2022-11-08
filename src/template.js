f36274a3008b65e6b0132dd56406061f2378c709
imran-rassi <imran.rassi.2020@gmail.com>
2022-11-08 14:37:42 -0500

    finish the template.js

--- src/template.js
+++ src/template.js
@@ -112,4 +112,49 @@ html.push(team
 
 };
 
+// Creating the main HTML //
+module.exports = team => {
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Our Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather|Muli:300" rel="stylesheet">
+        <link rel="stylesheet" href="../dist/style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    
+    <!-- adding the first header -->
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="team-profile-header col-12 mb-3 team-profile jumbotron">
+                    <h1 class="team-profile-title text-center"> My Team </h1>
+                </div>
+            </div>
+        </div>
+    
+        <div class="container">
+    
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center mt-5">
+                    ${generateTeam(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html> 
+
+  `;
+
+};
+
 
